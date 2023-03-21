8e110a8c86402093ca921b16085fc69b3213382d
Nikita Samarin <nsamarin@users.noreply.github.com>
2023-03-21 09:03:11 -0700

    Bump to 0.3.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(README, 'r') as f:
 
 setup(
     name='google-play-scraper-py',
-    version='0.3.0',
+    version='0.3.1',
     description='Scrape application data from the Google Play store.',
     long_description=long_description,
     long_description_content_type='text/markdown',
